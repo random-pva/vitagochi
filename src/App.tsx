24ec0fa30eae1b42ef6b05df96431cc491f6104d
Vladimir Petrov <v.petrov@notamedia.ru>
2023-03-07 20:45:40 +0400

    First steps

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import cat from './assets/cat.png'
+import goose from './assets/goose.png'
 import './App.css'
 
 function App() {
@@ -8,25 +9,25 @@ function App() {
   return (
     <div className="App">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        <h1>Выберите питомца</h1>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={cat} className="logo react" alt="React logo" />
         </a>
         <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={goose} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <footer id="footer">
+        <div>&copy; Random for kids</div>
+        <a href="https://www.flaticon.com/free-icons/goose" title="goose icons">Goose icons created by Smashicons - Flaticon</a>
+        <a href="https://www.flaticon.com/free-icons/cat" title="cat icons">Cat icons created by Freepik - Flaticon</a>
+      </footer>
     </div>
   )
 }
