a138b7360c020c0cb750d7756ed9f4fa860409a2
Vladimir Petrov <v.petrov@notamedia.ru>
2023-03-08 13:05:15 +0400

    Drag n drop test 2

--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -1,9 +1,11 @@
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
+import { TouchBackend } from 'react-dnd-touch-backend';
 
 import { useStore } from 'components/StoreProvider/StoreProvider';
 import SelectAnimal from 'components/SelectAnimal/SelectAnimal';
 import Life from 'components/Life/Life';
+
 
 function Game() {
   const [store, setStore] = useStore();
@@ -12,7 +14,7 @@ function Game() {
 
   return (
     <div>
-      <DndProvider backend={HTML5Backend}>
+      <DndProvider backend={TouchBackend} options={{ enableMouseEvents: true }}>
         {(animal ? <Life /> : <SelectAnimal />)}
       </DndProvider>
     </div>
